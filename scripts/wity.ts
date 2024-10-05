5ac18bede31695719142e30c40a2e0f42572474d
in-the-ocean <lucyzmj0914@gmail.com>
2024-10-05 13:35:26 -0400

    api call to get channel info

--- scripts/wity.ts
+++ scripts/wity.ts
@@ -1,4 +1,21 @@
+const YOUTUBE_URL = "https://www.youtube.com";
+const GOOGLE_API = "https://www.googleapis.com";
+const API_KEY = "AIzaSyDGU2Js5itra5Tcl9JkvhZFmIYSJFVV_fM";
+
+const showProfileIfAvailable = (event) => {
+    const target = event.target;
+    if (target && target.tagName == "A" && target.href && target.href.startsWith(YOUTUBE_URL + "/@")) {
+        let channelHandle = target.href.slice(target.href.search("@"));
+        console.log("usernameis", channelHandle)
+        fetch(GOOGLE_API + `/youtube/v3/channels?part=contentDetails&key=${API_KEY}&forHandle=${channelHandle}`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+            })
+    }
+}
 
 window.addEventListener("load", () => {
     console.log("WITY onload");
+    document.addEventListener("mouseover", showProfileIfAvailable);
 })
