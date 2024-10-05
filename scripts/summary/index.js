84487748e7015674378b2c2b23defb5432be4574
rainyuxuan <yu2001xuan@outlook.com>
2024-10-05 18:14:56 -0400

    Added UpStash cache

--- scripts/summary/index.js
+++ scripts/summary/index.js
@@ -2,47 +2,57 @@
  * @typedef {Object} PlaylistItem
  * @property {string} title - The title of the playlist item.
  * @property {string} description - The description of the playlist item.
+ * @property {string} publishedAt - The published date of the playlist item.
  */
 
-/**
- * Parse a playlist item into a string.
- * @param {*} item - The playlist item to parse.
- * @returns {string} - The stringified playlist item.
- */
-function parsePlaylistItem(item) {
-  const { title, description, publishedAt } = item;
+class AiSummary {
+  constructor(config) {
+    this.config = config;
+    this.cache = new UpStash(config.UPSTASH_API, config.UPSTASH_TOKEN);
+    this.llm = new OpenAI(config);
+  }
 
-  // Remove new lines and extra spaces from the description.
-  const cleanedDescription = description
-    .replace(/\n/g, " ")
-    .replace(/\s+/g, " ")
-    // Remove repeated characters (>3 times) from the description.
-    .replace(/(\w)\1{3,}/g, "$1")
-    // Remove urls and license codes from the description.
-    .replace(/(https?:\/\/[^\s]+)/g, "")
-    .replace(/License code: [A-Z0-9]+/g, "");
+  /**
+   * Parse a playlist item into a string.
+   * @private
+   * @param {PlaylistItem} item - The playlist item to parse.
+   * @returns {string} The stringified playlist item.
+   */
+  parsePlaylistItem(item) {
+    const { title, description, publishedAt } = item;
 
-  return JSON.stringify({
-    title,
-    description: cleanedDescription,
-    publishedAt,
-  });
-}
+    // Remove new lines and extra spaces from the description.
+    const cleanedDescription = description
+      .replace(/\n/g, " ")
+      .replace(/\s+/g, " ")
+      // Remove repeated characters (>3 times) from the description.
+      .replace(/(\w)\1{3,}/g, "$1")
+      // Remove urls and license codes from the description.
+      .replace(/(https?:\/\/[^\s]+)/g, "")
+      .replace(/License code: [A-Z0-9]+/g, "");
 
-/**
- * Generate summary of a YouTube Channel based on a list of playlist items.
- * @param {string} channelName - The name of the channel.
- * @param {Array.<PlaylistItem>} playlistItems - The list of playlist items.
- * @returns {Promise<string>} - The summary of the YouTube Channel.
- */
-async function summarizePlaylist(channelName, playlistItems) {
-  const stringifiedItems =
-    `Channel name: ${channelName} \nPlaylist items: \n` +
-    playlistItems.map(parsePlaylistItem).join("\n");
+    return JSON.stringify({
+      title,
+      description: cleanedDescription,
+      publishedAt,
+    });
+  }
 
-  console.log(stringifiedItems);
+  /**
+   * Generate summary of a YouTube Channel based on a list of playlist items.
+   * @private
+   * @param {string} channelName - The name of the channel.
+   * @param {Array.<PlaylistItem>} playlistItems - The list of playlist items.
+   * @returns {Promise<string>} The summary of the YouTube Channel.
+   */
+  async summarizePlaylist(channelName, playlistItems) {
+    const stringifiedItems =
+      `Channel name: ${channelName} \nPlaylist items: \n` +
+      playlistItems.map(this.parsePlaylistItem).join("\n");
 
-  const sysPrompt = `
+    console.log(stringifiedItems);
+
+    const sysPrompt = `
     You are a content analyst with expertise in understanding YouTube Channels 
     and their audiences. I will provide you with a list of video 
     titles, descriptions, and publish dates from a YouTuber's recent uploads. 
@@ -51,8 +61,36 @@ async function summarizePlaylist(channelName, playlistItems) {
     and recent trends. I want someone who has never watched their channel to get 
     a clear, interesting impression of the Channel in 3-5 sentences.`;
 
-  const openai = new OpenAI();
-  const completionResult = await openai.completion(sysPrompt, stringifiedItems);
+    const completionResult = await this.llm.completion(
+      sysPrompt,
+      stringifiedItems
+    );
 
-  return completionResult.choices[0].message.content;
+    return completionResult.choices[0].message.content;
+  }
+
+  /**
+   * Get the summary of a YouTube Channel.
+   * @param {string} channelId - The ID of the YouTube Channel.
+   * @param {string} channelName - The name of the YouTube Channel.
+   * @param {Array.<PlaylistItem>} playlistItems - The list of playlist items.
+   * @returns {Promise<string>} The summary of the YouTube Channel.
+   */
+  async getChannelSummary(channelId, channelName, playlistItems) {
+    // Check cache for existing channelId
+    const cacheKey = `channel-summary:${channelId}`;
+    const cachedSummary = await this.cache.getCache(cacheKey);
+
+    if (cachedSummary) {
+      return cachedSummary;
+    } else {
+      // Generate summary
+      const summary = await this.summarizePlaylist(channelName, playlistItems);
+
+      // Cache the summary
+      if (summary) this.cache.setCache(cacheKey, summary);
+
+      return summary;
+    }
+  }
 }
