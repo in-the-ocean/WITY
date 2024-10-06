36e290718dd678a9535ce412bad9f2c36a5138f1
HongruiTang <xiaotangthr@gmail.com>
2024-10-05 22:49:58 -0400

    fix: fixed undefined

--- scripts/ui.js
+++ scripts/ui.js
@@ -140,7 +140,7 @@ class UserProfileCard {
         console.log("Canvas", canvas.width, canvas.height);
 
         let wordCount = this.countWords(videoTitles, description);
-        WordCloud(canvas, { list: Object.entries(wordCount).map(([text, weight]) => ({text, weight})) });
+        WordCloud(canvas, { list: Object.entries(wordCount) });
 
     }
 
